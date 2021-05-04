d6296a063089b66e2fe3b4d7cafcd7bb347da740
John_Lamy <johnclamy@gmail.com>
2021-05-04 16:39:47 +0100

    styled the upper navbar

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,8 +1,10 @@
 import styles from '../../styles/Layout.module.css'
+import UpperNavbar from './UpperNavbar'
 
 export default function Layout({ children }) {
   return (
     <div className={styles.wrapper}>
+      <UpperNavbar />
       {children}
     </div>
   )
