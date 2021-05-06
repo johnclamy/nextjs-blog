29d0981b7f362eb3d706a277e00845b824cc16b6
John_Lamy <johnclamy@gmail.com>
2021-05-06 18:24:18 +0100

    removed the banner

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,14 +1,12 @@
 import styles from '../../styles/Layout.module.css'
 import UpperNavbar from './UpperNavbar'
 import Header from './Header'
-import Banner from './Banner'
 
 export default function Layout({ children }) {
   return (
     <div className={styles.wrapper}>
       <UpperNavbar />
       <Header />
-      <Banner />
       {children}
     </div>
   )
