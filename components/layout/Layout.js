997670da3c59b21817f00a9913203897dcb75dd9
John_Lamy <johnclamy@gmail.com>
2021-05-06 11:20:33 +0100

    setup banner images

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,12 +1,14 @@
 import styles from '../../styles/Layout.module.css'
 import UpperNavbar from './UpperNavbar'
 import Header from './Header'
+import Banner from './Banner'
 
 export default function Layout({ children }) {
   return (
     <div className={styles.wrapper}>
       <UpperNavbar />
       <Header />
+      <Banner />
       {children}
     </div>
   )
