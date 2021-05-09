66b143da32d095dfbf0015a2ae333fb77ab8e24e
John_Lamy <johnclamy@gmail.com>
2021-05-09 10:28:25 +0100

    added hero to home page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-export default function Home() {
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Hero from '../components/layout/Hero'
+
+export default function Home() {                                                                                                                 
   return (
-    <div>
-      testing the home page....
-    </div>
+    <Row>
+      <Col md={{ span: 10, offset: 1 }}>
+        <Hero />
+      </Col>
+    </Row>
   )
 }
