82db2e726e5c8908fed69b51e3c5d99a8682244b
John_Lamy <johnclamy@gmail.com>
2021-05-10 15:00:01 +0100

    add content component

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,16 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import Hero from '../components/layout/Hero'
+import Content from '../components/layout/Content'
 
 export default function Home() {                                                                                                                 
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
         <Hero />
+        <Content>
+          posts go here ...
+        </Content>
       </Col>
     </Row>
   )
