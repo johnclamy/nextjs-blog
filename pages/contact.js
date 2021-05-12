ee334672c73f127a7de62af4607a62dbbb37e3a6
John_Lamy <johnclamy@gmail.com>
2021-05-12 18:44:53 +0100

    add wrapper section to contact

--- pages/contact.js
+++ pages/contact.js
@@ -3,12 +3,21 @@ import Col from 'react-bootstrap/Col'
 
 import Content from '../components/layout/Content'
 
-export default function Home() {                                                                                                                 
+export default function Contact() {                                                                                                                 
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
         <Content>
-          contact info here...
+          <section className='bg-white mt-3 py-4 px-3'>
+            <Row>
+              <Col>
+                <header>
+                  <h1 className='display-4'>Contact Me</h1>
+                  <hr />
+                </header>
+              </Col>
+            </Row>
+          </section>
         </Content>
       </Col>
     </Row>
