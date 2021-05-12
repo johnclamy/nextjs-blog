f7bd6adb5c1770033d6f8082c7d4568877a4e4a0
John_Lamy <johnclamy@gmail.com>
2021-05-12 23:56:11 +0100

    styled contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,14 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import {
+  FaEnvelope,
+  FaLinkedin
+} from 'react-icons/fa'
 
 import Content from '../components/layout/Content'
 import HeaderPage from '../components/layout/HeaderPage'
+import { LINKED_IN } from '../components/SocialIcons'
+
 
 export default function Contact() {                                                                                                                 
   return (
@@ -15,10 +21,23 @@ export default function Contact() {
                 <HeaderPage>contact me</HeaderPage>
               </Col>
             </Row>
-            <Row >
-              <Col>LinkedIn page</Col>
-              <Col>Facebook page</Col>
-              <Col>Email address</Col>
+            <Row className='contact-page-links'>
+              <Col md={12} lg={4}>
+                <section classsName='contact-page-links-item'>
+                  <a className='text-white' href={LINKED_IN}>
+                    <div className='text-center'><FaLinkedin /></div>
+                    <h1 className='h3 text-center'>LinkedIn</h1>
+                    <p className='lead text-center'>Official contact page</p>
+                  </a>
+                </section>
+              </Col>
+              <Col md={12} lg={4}>
+                <section classsName='contact-page-links-item'>
+                  <div className='text-center'><FaEnvelope /></div>
+                  <h1 className='h3 text-center'>Email</h1>
+                  <p className='lead text-center'>tyronejstewart@yahoo.co.uk</p>
+                </section>
+              </Col>
             </Row>
           </section>
         </Content>
