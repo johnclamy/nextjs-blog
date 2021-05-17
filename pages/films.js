97df3a4e1613d3106af09a06cc2ae2ba9f43e84b
John_Lamy <johnclamy@gmail.com>
2021-05-17 23:12:27 +0100

    add film component to film page

--- pages/films.js
+++ pages/films.js
@@ -1,5 +1,12 @@
+import { Row, Col } from 'react-bootstrap'
+import FilmList from '../components/FilmList'
+
 export default function Films() {
   return (
-    <div>Film's page</div>
+    <Row>
+      <Col md={{ span: 10, offset: 1 }}>
+        <FilmList />
+      </Col>
+    </Row>
   )
 }
