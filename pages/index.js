62a1ae2f445d8c81a30246ce103b88de4c97378c
John_Lamy <johnclamy@gmail.com>
2021-05-19 15:34:39 +0100

    removed hello.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import { createClient } from 'contentful'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import Hero from '../components/layout/Hero'
 import Content from '../components/layout/Content'
 import PostList from '../components/PostList'
+
+export async function getStaticProps() {}
 
 export default function Home() {                                                                                                                 
   return (
