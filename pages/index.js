59ed2c7465dbd7e0caf0c2a9b113205ed07893c9
John_Lamy <johnclamy@gmail.com>
2021-05-19 19:15:35 +0100

    get posts from contentful

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import { createClient } from 'contentful'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 import Hero from '../components/layout/Hero'
 import Content from '../components/layout/Content'
 import PostList from '../components/PostList'
+import client from './api/contentful'
 
-// export async function getStaticProps() {}
+export async function getStaticProps() {
+  const res = await client.getEntries({ content_type: 'post' })
+  return {
+    props: {
+      posts: res.items
+    }
+  }
+}
 
-export default function Home() {                                                                                                                 
+export default function Home({ posts }) {
+  console.log(posts)                                                                                                             
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
