37f751b9d585476aa4832fc451b12664b5f78f9b
John_Lamy <johnclamy@gmail.com>
2021-05-20 08:45:27 +0100

    setup link for slug

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,12 +1,11 @@
 import Card from 'react-bootstrap/Card'
-// import Link from 'next/link'
+import Link from 'next/link'
 import moment from 'moment'
 
 export default function PostCard({ post }) {
-
   return (
     <Card className='app_post_card'>
-      <Card.Img variant='top' src={post.imgHeader} alt={post.title} />
+      <Card.Img variant='top' src={`https:${post.image.fields.file.url}`} alt={post.title} />
       <Card.Body className='app_post_card_body'>
         <Card.Text className='app_post_card_topic'>{post.topic}</Card.Text>
         <Card.Title className='app_post_card_title'>{post.title}</Card.Title>
@@ -14,10 +13,12 @@ export default function PostCard({ post }) {
           <span className='app_post_card_subtitle_prefix'>posted on</span> {moment(post.createdAt).format('LL')}
           <span className='app_post_card_subtitle_postfix'> by</span> {post.author}
         </Card.Subtitle>
-        <Card.Text className='app_post_card_text'>{subPostString(post.text)}...</Card.Text>
+        <Card.Text className='app_post_card_text'>{subPostString(post.message)}...</Card.Text>
       </Card.Body>
       <div className='text-center'>
-        <button className='app_post_card_btn app_post_card_submit'>read more</button>
+        <Link href={`/posts/${post.slug}`} passHref>
+          <button className='app_post_card_btn app_post_card_submit'>read more</button>
+        </Link>
       </div>
     </Card>
   )
