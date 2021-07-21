41ccec9083fa9748b04e760d045beb9a6441d7c6
John_Lamy <johnclamy@gmail.com>
2021-07-21 13:24:07 +0100

    add id for routing

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,13 +3,15 @@ import Card from 'react-bootstrap/Card'
 import Link from 'next/link'
 import moment from 'moment'
 
-export default function PostCard({ post }) {
+export default function PostCard({ id, post }) {
   return (
     <Card className='app_post_card'>
       <Card.Img variant='top' src={`https:${post.image.fields.file.url}`} alt={post.title} />
       <Card.Body className='app_post_card_body'>
         <Card.Text className='app_post_card_topic'>{post.topic}</Card.Text>
-        <Card.Title className='app_post_card_title'>{post.title}</Card.Title>
+        <Card.Title className='app_post_card_title'>
+          <Link href={`/posts/${id}`}><a>{post.title}</a></Link>
+        </Card.Title>
         <Card.Subtitle className='app_post_card_subtitle'>
           <span className='app_post_card_subtitle_prefix'>posted on</span> {moment(post.createdAt).format('LL')}
           <span className='app_post_card_subtitle_postfix'> by</span> {post.author}
