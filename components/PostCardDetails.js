c5ac49ec7c8bc94bdc0da15a175c73e7cbb18ba7
John_Lamy <johnclamy@gmail.com>
2021-07-23 20:41:11 +0100

    edit details with post prop

--- components/PostCardDetails.js
+++ components/PostCardDetails.js
@@ -5,15 +5,15 @@ import moment from 'moment'
 export default function PostCardDetails({ post }) {
   return (
     <Card className='app_post_card'>
-      <Card.Img variant='top' src={`https:${post.image.fields.file.url}`} alt={post.title} />
+      <Card.Img variant='top' src={`https:${post.fields.image.fields.file.url}`} alt={post.fields.title} />
       <Card.Body className='app_post_card_body'>
-        <Card.Text className='app_post_card_topic'>{post.topic}</Card.Text>
-        <Card.Title className='app_post_card_title'>{post.title}</Card.Title>
+        <Card.Text className='app_post_card_topic'>{post.fields.topic}</Card.Text>
+        <Card.Title className='app_post_card_title'>{post.fields.title}</Card.Title>
         <Card.Subtitle className='app_post_card_subtitle'>
-          <span className='app_post_card_subtitle_prefix'>posted on</span> {moment(post.createdAt).format('LL')}
-          <span className='app_post_card_subtitle_postfix'> by</span> {post.author}
+          <span className='app_post_card_subtitle_prefix'>posted on</span> {moment(post.sys.createdAt).format('LL')}
+          <span className='app_post_card_subtitle_postfix'> by</span> {post.fields.author}
         </Card.Subtitle>
-        <Card.Text className='app_post_card_text'>{documentToReactComponents(post.message)}</Card.Text>
+        <Card.Text className='app_post_card_text'>{documentToReactComponents(post.fields.message)}</Card.Text>
       </Card.Body>
     </Card>
   )
