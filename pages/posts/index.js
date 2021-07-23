efbc1c5de4cca3f50530ca6256a76eb2b7bafae0
John_Lamy <johnclamy@gmail.com>
2021-07-23 20:44:06 +0100

    get data to pass down prop chain

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,11 +1,22 @@
 import { Row, Col } from 'react-bootstrap'
-import PostList from '../../components/PostList'
 
-export default function Posts() {
+import PostList from '../../components/PostList'
+import client from '../api/contentful'
+
+export async function getStaticProps() {
+  const res = await client.getEntries({ content_type: 'post' })
+  return {
+    props: {
+      posts: res.items
+    }
+  }
+}
+
+export default function Posts({ posts }) {
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
-        <PostList />
+        <PostList posts={posts} />
       </Col>
     </Row>
   )
