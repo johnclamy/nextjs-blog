cec43aa68190bf29a17415637afe0e20cffef6d8
John_Lamy <johnclamy@gmail.com>
2021-08-04 00:21:12 +0100

    fetch film details

--- pages/films/[slug].js
+++ pages/films/[slug].js
@@ -1,11 +1,40 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 
-export default function () {
+import FilmDetailsCard from '../../components/FilmDetailsCard'
+import client from '../api/contentful'
+
+export const getStaticPaths = async () => {
+  const rslt = await client.getEntries({ content_type: 'film' })
+  const paths = rslt.items.map(item => {
+    return {
+      params: { slug: item.fields.youTubeId }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const { items } = await client.getEntries({
+    content_type: 'film',
+    'fields.youTubeId': params.slug
+  })
+
+  return {
+    props: { film: items[0] }
+  }
+}
+
+export default function ({ film }) {
+  console.log(film)
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
-        <p>Film details page goes here...</p>
+        <h3>Film details goes here...</h3>
       </Col>
     </Row>
   )
