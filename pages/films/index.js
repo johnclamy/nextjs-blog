0fdf4bbe9ff25597503889f97142500f7a73db24
John_Lamy <johnclamy@gmail.com>
2021-08-03 19:12:27 +0100

    fetch films from api end point

--- pages/films/index.js
+++ pages/films/index.js
@@ -1,11 +1,22 @@
 import { Row, Col } from 'react-bootstrap'
-import FilmList from '../../components/FilmList'
 
-export default function Films() {
+import FilmList from '../../components/FilmList'
+import client from '../api/contentful'
+
+export async function getStaticProps() {
+  const res = await client.getEntries({ content_type: 'film' })
+
+  return {
+    props: { films: res.items }
+  }
+}
+
+export default function Films({ films }) {
+  console.log(films)
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
-        <FilmList />
+        <FilmList films={films} />
       </Col>
     </Row>
   )
