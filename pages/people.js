9decf3701636f35efdbb58637dd92adcce372039
John_Lamy <johnclamy@gmail.com>
2021-08-06 19:58:20 +0100

    fetch data from Contentful

--- pages/people.js
+++ pages/people.js
@@ -1,11 +1,23 @@
+import { getFontDefinitionFromManifest } from 'next/dist/next-server/server/font-utils'
 import { Row, Col } from 'react-bootstrap'
-import PeopleList from '../components/PeopleList'
 
-export default function People() {
+import PeopleList from '../components/PeopleList'
+import client from './api/contentful'
+
+export async function getStaticProps() {
+  const rslt = await client.getEntries({ content_type: 'people' })
+
+  return {
+    props: { people: rslt.items }
+  }
+}
+
+export default function People({ people }) {
+  console.log(people)
   return (
     <Row>
       <Col md={{ span: 10, offset: 1 }}>
-        <PeopleList />
+        <PeopleList people={people} />
       </Col>
     </Row>
   )
