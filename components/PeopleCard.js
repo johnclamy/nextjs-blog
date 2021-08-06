300ce49f0cb28e5ac3d0cfc2cb5c9371caa9456a
John_Lamy <johnclamy@gmail.com>
2021-08-06 21:18:06 +0100

    styled people's list

--- components/PeopleCard.js
+++ components/PeopleCard.js
@@ -1,45 +1,40 @@
 import { Card, Media, Image, ListGroup } from 'react-bootstrap'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 export default function PeopleCard({ person }) {
-  const imdb = person.imdb ? person.imdb : ''
-  const initial = person.middleName ? person.middleName : ''
-  const name = `${person.firstName} ${(initial ? initial : '')} ${person.lastName}`
-  const title = imdb ? (<Card.Link href={imdb}>{name}</Card.Link>) : name
-  const image = person.profilePic ? person.profilePic : '/images/people/placeholder.gif'
+  const { url } = person.profilePicture.fields.file
+  const imdb = person.imDbUrl ? person.imDbUrl : ''
+  const title = imdb ? (<Card.Link href={imdb}>{person.name}</Card.Link>) : person.name
   const imageComponent = (
     imdb
       ? <a href={imdb}>
           <Image rounded
             className='app-people-card-img'
-            src={image}
+            src={`https:${url}`}
             alt='Profile picture'
           />
         </a>
       : <Image rounded
           className='app-people-card-img'
-          src={image}
+          src={`https:${url}`}
           alt='Profile picture'
         />
   )
 
   return (
     <ListGroup.Item className='app-people-card-item'>
-      <Media>
-        {imageComponent}
-        <Media.Body>
-          <Card>
-            <Card.Body>
-              <Card.Title>
-                <span className='app-people-card-name'>{title}</span>&nbsp;
-                <span className='app-people-card-role'>
-                  ({person.mainRole})
-                </span>
-              </Card.Title>
-              <Card.Text>{person.bio}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Media.Body>
-      </Media>
+      <Card>
+        <Card.Header>{imageComponent}</Card.Header>
+          <Card.Body>
+            <Card.Title>
+              <span className='app-people-card-name'>{title}</span>&nbsp;
+              <span className='app-people-card-role'>
+                ({person.mainRole})
+              </span>
+            </Card.Title>
+            <Card.Text>{documentToReactComponents(person.bio)}</Card.Text>
+          </Card.Body>
+      </Card>
     </ListGroup.Item>
   )
 }
