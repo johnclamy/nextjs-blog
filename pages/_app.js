c22bfb31daec348a40c6fe8ed5eaddac30574a1a
John_Lamy <johnclamy@gmail.com>
2021-08-11 15:03:14 +0100

    add router events

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import Router from 'next/router'
+
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout'
 
 function MyApp({ Component, pageProps }) {
+  Router.events.on('routeChangeStart', (url) => {
+    console.log('Route change started...')
+  })
+
+  Router.events.on('routeChangeComplete', (url) => {
+    console.log('Route change is complete...')
+  })
+
   return <Layout><Component {...pageProps} /></Layout>
 }
 
