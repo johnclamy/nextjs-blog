894a9b7b4a72435b70520e3142a337a807fec339
John_Lamy <johnclamy@gmail.com>
2021-08-11 12:19:13 +0100

    add card details for work data

---
+++ components/WorkCard.js
@@ -0,0 +1,40 @@
+import { Row, Col } from 'react-bootstrap'
+import Card from 'react-bootstrap/Card'
+import ReactPlayer from 'react-player'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+
+const YOUTUBE_URL = 'https://www.youtube.com/watch?v='
+
+export default function WorkCard({ workItem }) {
+  console.log(workItem)
+  return (
+    <Row>
+      <Col md={6}>
+        <div className='app_pages_work_container'>
+          <Card style={{ height: '21rem' }}>
+            <Card.Body>
+              <ReactPlayer
+                height='100%'
+                width='100%'
+                url={`${YOUTUBE_URL}${workItem.youTubeId}`}
+              />
+            </Card.Body>
+          </Card>
+        </div>
+      </Col>
+      <Col md={6}>
+        <div className='app_pages_work_container'>
+          <Card>
+            <Card.Body>
+              <Card.Title>{workItem.title}</Card.Title>
+              {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
+              <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
+              {/* <Card.Link>Card Link</Card.Link>
+              <Card.Link>Another Link</Card.Link> */}
+            </Card.Body>
+          </Card>
+        </div>
+      </Col>
+    </Row>
+  )
+}
