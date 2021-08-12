26592fe6f8653f0987b8535227e951f84f93d240
John_Lamy <johnclamy@gmail.com>
2021-08-12 11:06:49 +0100

    ass spinner

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -11,6 +11,7 @@ export default function Layout({ children }) {
         <title>Jemsea Films | Tyrone J Stewart short films and work blog</title>
         <meta property="og:title" content="Jemsea Films title" key="title" />
         <meta name='keywords' content='Tyrone J Stewart short films film editing work blog' />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css" integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
       </Head>
       <Container fluid>
         <Row>
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 import Router from 'next/router'
+import NProgress from 'nprogress'
 
 import '../styles/globals.css'
 import Layout from '../components/layout/Layout'
 
 function MyApp({ Component, pageProps }) {
-  Router.events.on('routeChangeStart', (url) => {
-    console.log('Route change started...')
-  })
-
-  Router.events.on('routeChangeComplete', (url) => {
-    console.log('Route change is complete...')
-  })
+  Router.events.on('routeChangeStart', (url) => NProgress.start())
+  Router.events.on('routeChangeComplete', (url) => NProgress.done())
 
   return <Layout><Component {...pageProps} /></Layout>
 }
