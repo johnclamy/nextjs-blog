fcf59103fc058b66e967e7cfba148fb37f6e4027
John_Lamy <johnclamy@gmail.com>
2021-08-14 15:18:04 +0100

    adapt btns for small screens

--- components/PostCardButton.js
+++ components/PostCardButton.js
@@ -1,7 +1,7 @@
 export default function PostCardButton(props) {
   return (
     <button
-      className='app_post_card_btn app_post_card_submit'
+      className='app-post-card-btn app-post-card-submit'
       onClick={props.onClick}
     >
       {props.children}
