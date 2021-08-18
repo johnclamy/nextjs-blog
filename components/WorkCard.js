b47231266d58c62b461f040c5e1835d26eb867b0
John_Lamy <johnclamy@gmail.com>
2021-08-18 15:33:26 +0100

    set content in single card

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -1,5 +1,5 @@
 import { Row, Col } from 'react-bootstrap'
-import Card from 'react-bootstrap/Card'
+import { Card, ListGroup, ListGroupItem, Link } from 'react-bootstrap'
 import ReactPlayer from 'react-player'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
@@ -7,32 +7,30 @@ const YOUTUBE_URL = 'https://www.youtube.com/watch?v='
 
 export default function WorkCard({ workItem }) {
   return (
-    <Row>
-      <Col md={6}>
-        <div className='work-card-container'>
-          <Card className='work-card'>
-            <Card.Body>
-              <ReactPlayer
-                height='100%'
-                width='100%'
-                url={`${YOUTUBE_URL}${workItem.youTubeId}`}
-              />
-            </Card.Body>
-          </Card>
-        </div>
-      </Col>
-      <Col md={6}>
-        <div className='work-card-container'>
-          <Card className='work-card work-card-text'>
-            <Card.Body>
-              <Card.Title>{workItem.title}</Card.Title>
-              {/* <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle> */}
-              <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
-              {/* <Card.Link>Card Link</Card.Link>
-              <Card.Link>Another Link</Card.Link> */}
-            </Card.Body>
-          </Card>
-        </div>
+    <Row className='d-flex justify-content-center '>
+      <Col  xs={{span: 12}} sm={{span: 12}} md={{span: 12}} lg={{ span: 8, offset: 2 }}>
+        <Card>
+          <Card.Body className='d-flex justify-content-center work-card'>
+            <ReactPlayer
+              height='100%'
+              width='100%'
+              url={`${YOUTUBE_URL}${workItem.youTubeId}`}
+            />
+          </Card.Body>
+          <Card.Body>
+            <Card.Title>{workItem.title}</Card.Title>
+            <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroupItem>Cras justo odio</ListGroupItem>
+            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+            <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          </ListGroup>
+          <Card.Body>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
       </Col>
     </Row>
   )
