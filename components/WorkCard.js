10f694c4b53bc448f5498d99f2cab4402578d264
John_Lamy <johnclamy@gmail.com>
2021-08-18 15:50:55 +0100

    superfluous row removed

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -7,31 +7,29 @@ const YOUTUBE_URL = 'https://www.youtube.com/watch?v='
 
 export default function WorkCard({ workItem }) {
   return (
-    <Row className='d-flex justify-content-center '>
-      <Col  xs={{span: 12}} sm={{span: 12}} md={{span: 12}} lg={{ span: 8, offset: 2 }}>
-        <Card>
-          <Card.Body className='d-flex justify-content-center work-card'>
-            <ReactPlayer
-              height='100%'
-              width='100%'
-              url={`${YOUTUBE_URL}${workItem.youTubeId}`}
-            />
-          </Card.Body>
-          <Card.Body>
-            <Card.Title>{workItem.title}</Card.Title>
-            <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroupItem>Cras justo odio</ListGroupItem>
-            <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-            <ListGroupItem>Vestibulum at eros</ListGroupItem>
-          </ListGroup>
-          <Card.Body>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+    <Col  xs={{span: 12}} sm={{span: 12}} md={{span: 12}} lg={{ span: 8, offset: 2 }}>
+      <Card>
+        <Card.Body className='d-flex justify-content-center work-card'>
+          <ReactPlayer
+            height='100%'
+            width='100%'
+            url={`${YOUTUBE_URL}${workItem.youTubeId}`}
+          />
+        </Card.Body>
+        <Card.Body>
+          <Card.Title>{workItem.title}</Card.Title>
+          <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          <ListGroupItem>Cras justo odio</ListGroupItem>
+          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">Card Link</Card.Link>
+          <Card.Link href="#">Another Link</Card.Link>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
