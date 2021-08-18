2baae8e5bff1bea593bd2965006b0eec1419777e
John_Lamy <johnclamy@gmail.com>
2021-08-18 16:27:10 +0100

    remove list and links

--- components/WorkCard.js
+++ components/WorkCard.js
@@ -20,15 +20,6 @@ export default function WorkCard({ workItem }) {
           <Card.Title>{workItem.title}</Card.Title>
           <Card.Text>{documentToReactComponents(workItem.description)}</Card.Text>
         </Card.Body>
-        <ListGroup className="list-group-flush">
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
-        </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
       </Card>
     </Col>
   )
